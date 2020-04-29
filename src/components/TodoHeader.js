bb34be0336d1c7f677a3842dfeff13aababfc5c9
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-30 01:00:27 +0300

    start new project

--- src/components/TodoHeader.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class TodoHeader extends Component{
-  render() {
-    return <h1 className="todoHeader">To-do List</h1>;
-  }
-}
-
-export default TodoHeader;
